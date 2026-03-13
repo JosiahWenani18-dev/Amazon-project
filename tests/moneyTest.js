ddfa7d79fc41cb1d243bfa24b41a8a7d263b6df0
JosiahWenani18-dev <josiahwenani18@gmail.com>
2026-03-14 00:37:28 +0300

    Create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,33 @@
+import formatCurreny from '../scripts/utils/money.js';
+
+console.log('test suite: formatCurrency');
+
+
+console.log('converts cents into dollars');
+if(formatCurreny(2095) === '20.95') {
+  console.log('passed');
+}else {
+  console.log('Failed');
+}
+
+console.log('works with zero');
+
+if(formatCurreny(0) === '0.00') {
+  console.log('passed');
+}else {
+  console.log('Failed');
+}
+
+console.log('rounds up to the nearest cents');
+if(formatCurreny(2000.5) === '20.01') {
+  console.log('passed');
+}else {
+  console.log('Failed');
+}
+
+console.log('rounds down to the nearest cents');
+if(formatCurreny(2000.4) === '20.00') {
+  console.log('passed');
+}else {
+  console.log('Failed');
+}
