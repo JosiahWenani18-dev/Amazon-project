39d9ee74e0712ca0c2b2e18e8deaf2958ebcfabc
JosiahWenani18-dev <josiahwenani18@gmail.com>
2026-04-19 16:03:24 +0300

    Load products in beforeAll hook

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,11 +1,20 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 
-productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 describe('test suit: rendeOrderSummary', () => {
-  it('displays the cart', () => {
+  productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+  productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  beforeAll ((done) => {
+    loadProducts (() => {
+    done();
+    });
+  });
+
+  beforeEach (() => {
+    spyOn(localStorage, 'setItem');
     document.querySelector('.js-test-container').innerHTML = `
     <div class="js-order-summary"></div>
     <div class="js-payment-summary"></div>`;
@@ -24,6 +33,10 @@ describe('test suit: rendeOrderSummary', () => {
     loadFromStorage();
 
     renderOrderSummary();
+  });
+
+/*
+  it('displays the cart', () => {
 
     expect(
     document.querySelectorAll('.js-cart-item-container').length
@@ -60,4 +73,5 @@ describe('test suit: rendeOrderSummary', () => {
 
     document.querySelector(`.js-delete-link-${productId1}`).click();
   })
+    */
 });
